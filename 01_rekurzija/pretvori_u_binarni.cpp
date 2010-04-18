c980048d7008053e963f7809af35fab06e1454ce
Ernad Husremovic <hernad@bring.out.ba>
2010-04-18 12:33:33 +0200

    pretvori integer u binarnu reprezentaciju

---
+++ 01_rekurzija/pretvori_u_binarni.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+void PretvoriUBinarni(int broj) {
+
+  if (broj <= 1) {
+     cout << broj;
+     return;
+  }
+
+  int bit = broj % 2;
+  broj /= 2;
+
+  PretvoriUBinarni(broj);
+
+  cout << bit;
+}
+
+
+int main()
+{
+
+   int broj = 0;
+
+   do {
+     cout << "unesite broj: ";
+     cin >> broj;
+ 
+     cout << "rezultat je " ;
+     PretvoriUBinarni(broj); 
+     cout << endl;
+
+
+     // http://www.sgi.com/tech/stl/bitset.html
+
+     bitset<16> binarno(broj);
+
+     cout << "provjera sa bitset: " << binarno;  
+
+     cout << "-------------------------------" << endl;
+   } while (broj>=0);
+}

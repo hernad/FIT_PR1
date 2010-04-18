ad969839fa4be277399dfa16951f378fcc8d33a1
Ernad Husremovic <hernad@bring.out.ba>
2010-04-18 12:04:35 +0200

    refactored

--- 01_rekurzija/suma_parnih_brojeva_do_100.cpp
+++ 01_rekurzija/suma_parnih_brojeva_do_100.cpp
@@ -4,46 +4,36 @@ using namespace std;
 
 int sumaParnih_do_100(int num) {
 
-  int rez, in_num = num;
+  int rez, plus_minus = 1;
 
-  if (in_num == 100) return 100;
+  if (num == 100) return 100;
 
-  if (in_num < 100) {
+  // oduzimaj
+  if (num > 100) plus_minus = -1;
 
-     if ( (in_num % 2) == 0) {
-          // paran npr. 98
+
+  if ( (num % 2) == 0) {
           cout << "--- debug dodajem: " << num << endl;
-          rez = sumaParnih_do_100(num + 2) + num ;
-     } else { 
+          rez = sumaParnih_do_100(num + plus_minus * 2 ) + num ;
+  } else { 
           // neparan npr. 99
-	  rez = sumaParnih_do_100(num + 1); 
-     }
-     
-  } else {
-
-     if ( (in_num % 2) == 0) {
-          // paran npr. 98
-          cout << "--- debug dodajem: " << num << endl;
-          rez = sumaParnih_do_100(num - 2) + num ;
-     } else { 
-          // neparan npr. 99
-	  rez = sumaParnih_do_100(num - 1); 
-     }
- 
+	  rez = sumaParnih_do_100(num + plus_minus); 
   }
-
+     
 }
 
 int main() {
    
  int sabrati;
 
- cout << "ver 0.01" << endl << endl;
+ cout << "ver 0.02 refactored" << endl << endl;
 
  cout << "unesi broj za sabiranje: ";
  cin >> sabrati;
 
  cout << "suma svih parnih brojeva izmedju " << sabrati << " i 100 je " << sumaParnih_do_100(sabrati);
+
+ cout << endl;
 
  return 0;
 }

d17d818d2a354f906070b4e7a0e5267798d34854
Ernad Husremovic <hernad@bring.out.ba>
2010-06-17 11:25:11 +0200

    dyn struct

---
+++ 11_struct_2/struct_dyn.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+struct Vozilo {
+    int godina;
+    int kubika;
+    char naziv[200];
+
+};
+
+int main()
+{
+
+
+   Vozilo *vozila;
+
+   char nazivi[2][100] = { "naz 1", "naz 2" };
+
+   vozila = new Vozilo[10];
+
+   cout << nazivi[0] << endl;
+
+   strcpy(vozila[5].naziv, nazivi[0]);
+   vozila[5].kubika = 1000;
+
+   cout << "naziv [5]= " << vozila[5].naziv << endl;
+   cout << "kubika " << vozila[5].kubika << endl;
+
+
+   cout << "naziv [9]= " << vozila[9].naziv << endl;
+   cout << "kubika " << vozila[9].kubika << endl;
+
+
+   cout << "naziv [10]= " << vozila[10].naziv << endl;
+   cout << "kubika " << vozila[10].kubika << endl;
+
+
+   return 0;
+}

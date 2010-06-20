7e3f0dc0c5198b331d0e140de718a4805aeb010c
Ernad Husremovic <hernad@bring.out.ba>
2010-06-20 23:10:42 +0200

    primjeri komandi

---
+++ parc_2_vjezbe/komande.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+
+#include <iomanip>
+
+#include <string.h>
+
+#include <fstream>
+
+using namespace std;
+
+void for_next() {
+
+cout << "for-next sa continue:" << endl;
+
+for (int i=1; i<10; i++) {
+ 
+   if (i%2 == 0)
+        continue;
+
+   cout << "prolazim kroz " << i << endl;
+}
+
+cout << "for-next sa break:" << endl;
+
+for (int i=1; i<10; i++) {
+ 
+   if (i==4)
+        break;
+
+   cout << "prolazim kroz " << i << endl;
+}
+
+cout << "by by from for-next" << endl;
+
+}
+
+
+typedef struct {
+  char  name[10];
+  int i;
+  int j;
+  double f;
+} TStruct1;
+ 
+
+void struct_1() {
+
+ TStruct1 s1;
+
+ strncpy(s1.name, "hernad", 10);
+ s1.i = 55;
+ s1.j = 44;
+ s1.f = 121.66;
+
+ cout << "s1 =" << s1.name << " " << s1.i << " " << s1.j <<  " " << s1.f << endl;
+
+ // ios::ate - at end
+ // ios:app - append i ios::trunc ne mogu zajedno !!
+
+ fstream fs1("fs1.bin", ios::binary | ios::in | ios::out | ios::trunc);
+
+ if (fs1.fail()) { 
+      cout << "belaj ...";
+      return;
+ }
+
+ fs1.write((char *) &s1, sizeof(s1));
+
+ strncpy(s1.name, "hernad2", 10);
+ s1.i = 99;
+
+ fs1.write((char *) &s1, sizeof(s1));
+
+
+ strncpy(s1.name, "hernad3", 10);
+ s1.i = 111;
+
+ fs1.write((char *) &s1, sizeof(s1));
+
+ strncpy(s1.name, "hernad4g", 10);
+ s1.i = 232;
+
+ fs1.write((char *) &s1, sizeof(s1));
+
+
+ fs1.seekg(ios::beg);
+
+ fs1.seekg(2 * sizeof(s1));
+
+ TStruct1 s2, s3;
+ fs1.read((char *) &s2, sizeof(s2));
+ fs1.read((char *) &s3, sizeof(s2));
+
+ 
+ cout << "s2 =" << s2.name << " " << s2.i << " " << s2.j <<  " " << s2.f << endl;
+ 
+ cout << "s3 =" << s3.name << " " << s3.i << " " << s3.j <<  " " << s3.f << endl;
+
+ fs1.close();
+
+}
+
+void getline_1() {
+
+char *temp = new char[10];
+
+cout << "unesite TACNO 10 znakova :";
+cin.getline(temp, 10);
+
+cout << "ukucano je :" << temp << endl;
+
+cout << "duzina stringa je " << strlen(temp) << endl;
+}
+
+int main() {
+
+//getline_1();
+
+// for_next();
+
+struct_1();
+
+return 0;
+}
+
